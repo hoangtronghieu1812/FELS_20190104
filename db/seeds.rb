48a772324e085fdfa33be0da4a910cd434195625
duongthao.1610 <duongthao.1610@gmail.com>
2019-01-08 16:41:12 +0700

    Show word list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!  name: "Dương Phương Thảo",
+              email: "duongthao@gmail.com",
+              phone: "09888888",
+              age: 22,
+              job: "Sinh viên"
+
+Course.create! name: "Basic 1",
+                description: "Description: Basic 1"
+
+Course.create! name: "Basic 2",
+                description: "Description: Basic 2"
+
+Course.create! name: "Basic 3",
+                description: "Description: Basic 3"
+
+Word.create! content: "Apple",
+              course_id: 1
+
+Word.create! content: "Cat",
+              course_id: 1
+
+Word.create! content: "Doctor",
+              course_id: 2
+
+Word_answer.create! content: "Quả táo",
+                    correct: true,
+                    word_id: 1
+
+Word_answer.create! content: "Quả cam",
+                    correct: false,
+                    word_id: 1
+
+Word_answer.create! content: "Quả ổi",
+                    correct: false,
+                    word_id: 1
+
+Word_answer.create! content: "Quả xoài",
+                    correct: false,
+                    word_id: 1
+
+Word_answer.create! content: "Con mèo",
+                    correct: true,
+                    word_id: 2
+
+Word_answer.create! content: "Con chó",
+                    correct: false,
+                    word_id: 2
+
+Word_answer.create! content: "Con bò",
+                    correct: false,
+                    word_id: 2
+
+Word_answer.create! content: "Con trâu",
+                    correct: false,
+                    word_id: 2
+
+Word_answer.create! content: "Bác sĩ",
+                    correct: true,
+                    word_id: 3
+
+Word_answer.create! content: "Nông dân",
+                    correct: false,
+                    word_id: 3
+
+Word_answer.create! content: "Giáo viên",
+                    correct: false,
+                    word_id: 3
+
+Word_answer.create! content: "Công an",
+                    correct: false,
+                    word_id: 3
+
