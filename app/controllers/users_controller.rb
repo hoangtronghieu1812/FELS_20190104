d6e5e75848cf1434bde17d5bda0c1207c4c5a926
hoangtronghieu <hoangtronghieu1812@gmail.com>
2019-01-22 09:55:09 +0700

    Handle Ajax with follow button, show user's follower/followings and memorised words

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_action :require_login
+
   def show
     @current_user = current_user
     @user = User.find_by id: params[:id]
+    require_details_from @user
   end
+
 end
