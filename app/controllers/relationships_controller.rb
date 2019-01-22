0b410cc3ec38d718067d923435f17341b2b1e42f
hoangtronghieu <hoangtronghieu1812@gmail.com>
2019-01-22 09:55:09 +0700

    Handle Ajax with follow button, show user's follower/followings and memorised words

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,6 +4,9 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by id: params[:followed_id]
     current_user.follow @user
+    @number_of_memorised_words = Word.with_learned(@user.id).count.to_s
+    @number_of_followers = @user.followers.count.to_s
+    @number_of_followings = @user.following.count.to_s
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -13,6 +16,9 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find_by(id: params[:id]).followed
     current_user.unfollow @user
+    @number_of_memorised_words = Word.with_learned(@user.id).count.to_s
+    @number_of_followers = @user.followers.count.to_s
+    @number_of_followings = @user.following.count.to_s
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_action :require_login
+
   def show
     @current_user = current_user
     @user = User.find_by id: params[:id]
+    @number_of_memorised_words = Word.with_learned(@user.id).count.to_s
+    @number_of_followers = @user.followers.count.to_s
+    @number_of_followings = @user.following.count.to_s
   end
 end
