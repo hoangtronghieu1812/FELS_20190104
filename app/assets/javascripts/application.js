4bcbc156a1189866b1314af4f0f215c3a051ed47
duongthao <duongthao.1610@gmail.com>
2019-02-27 17:23:52 +0700

    Custom rails admin: Dynamic form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,11 @@
 //= require jquery
 //= require popper
 //= require bootstrap
-//= require rails-ujs
+//= require jquery_ujs
+//= require jquery-ui
 //= require activestorage
 //= require turbolinks
 //= require course
 //= require create-lesson
+//= require word_answer
+
