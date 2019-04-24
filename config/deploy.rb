4c14352421be979da5e2face78e271844a8f22c6
hoangtronghieu <hoangtronghieu1812@gmail.com>
2019-04-24 16:20:42 +0700

    deploy ne

---
+++ config/deploy.rb
@@ -0,0 +1,71 @@
+set :application, 'fels_20190104'
+
+# Define where can Capistrano access the source repository
+
+# set :repo_url, 'https://github.com/[user name]/[application name].git'
+
+set :git
+set :repo_url, 'https://github.com/hoangtronghieu1812/FELS_20190104'
+
+# Define where to put your application code
+
+set :deploy_to, "/home/troublehfrom18/environment/deploy/FELS_20190104"
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_ruby, '2.6.0'
+
+set :pty, true
+
+set :format, :pretty
+
+set :passenger_restart_with_touch, true
+
+
+# Set the post-deployment instructions here
+
+# Once the deployment is complete, Capistrano
+
+# will begin performing them as described
+
+# To learn more about creating tasks
+
+# check out
+
+# http://capistranorb.com/
+
+# namespace: deploy do
+
+# desc 'Restart application'
+
+# task :restart do
+
+# on roles(:app), in: :sequence, wait: 5 do
+
+# # Your restart mechanism here, for example
+
+# execute :touch, release_path.join('tmp/restart.txt')
+
+# end
+
+# end
+
+# after :publishing, :restart
+
+# after :restart, :clear_cache do
+
+# on roles(:web), in: :groups, limit: 3, wait: 10 do
+
+# # Here we can do anything such as
+
+# # within release_path do
+
+# #   execute :rake, 'cache:clear'
+
+# # end
+
+# end
+
+# end
+
+# end
---
+++ config/deploy/production.rb
@@ -0,0 +1,25 @@
+# Define roles, user and IP address of deployment server
+
+# role :name, %{[user]@[IP adde.]}
+
+role :app, %w{troublehfrom18@10.0.37.78}
+role :web, %w{troublehfrom18@10.0.37.78}
+role :db,  %w{troublehfrom18@10.0.37.78}
+set :rails_env, :production
+set :branch, "master"
+# Define server(s)
+
+server '10.0.37.78', user: 'troublehfrom18', roles: %w{app db web}
+
+# SSH Options
+
+# See the example commented out section in the file
+
+# for more options
+
+# set :ssh_options, {
+#     forward_agent: false,
+#     auth_methods: %w(password),
+#     password: ENV["deploy_password"],
+#     user: 'troublehfrom18',
+# }
