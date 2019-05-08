ce97613853b912d769a3187f54e3114aa1d73282
hoangtronghieu <hoangtronghieu1812@gmail.com>
2019-05-08 10:12:50 +0700

    Xoa gem searchkick- lỗi port 9200

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,19 @@
-set :application, 'fels_20190104'
+set :application, 'fels'
 
 # Define where can Capistrano access the source repository
 
 # set :repo_url, 'https://github.com/[user name]/[application name].git'
 
-set :git, :scm
-set :repo_url, 'https://github.com/hoangtronghieu1812/FELS_20190104'
+set :repo_url, 'https://github.com/hoangtronghieu1812/FELS_20190104.git'
 
 # Define where to put your application code
 
-set :deploy_to, "/home/troublehfrom18/environment/deploy/FELS_20190104"
+set :deploy_to, "/var/www/fels/"
+set :migration_role, :app
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_ruby, '2.6.0'
-
-set :pty, true
-
-set :format, :pretty
-
-set :passenger_restart_with_touch, true
-
 
 # Set the post-deployment instructions here
 
@@ -69,3 +62,12 @@ set :passenger_restart_with_touch, true
 # end
 
 # end
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web, :db), in: :groups, limit: 3, wait: 10 do
+      run "cd /var/www/fels/current; bundle exec rake db:seed RAILS_ENV=production"
+    end
+  end
+
+end
